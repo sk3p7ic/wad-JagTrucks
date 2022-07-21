e33d3e991e936cd807435a4444c6a438247ac8e9
Ryan Kasper <rhinodino5628@gmail.com>
2022-07-21 14:46:59 -0500

    Added a small container not sure where to put yet.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,15 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 export const HomePage = ()=>{
     return <div>Hello World!</div>
+    function BlockofTitle(){
+        return(
+            <Container>
+                <Row>
+                    <Col md={{ span: 3, offset: 3}}>{'md={{span: 3, offset: 3}}'}</Col>
+                </Row>
+            </Container>
+        )
+    }
 }
