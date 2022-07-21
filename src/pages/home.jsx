921bb3dde13fded32f274feaa463ebdef685a85b
Ryan Kasper <rhinodino5628@gmail.com>
2022-07-21 17:43:52 -0500

    Fixed blockofTitle to connect properly.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,25 +2,28 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 
-function BlockofTitle() {
+function blockofTitle() {
   return (
-    <Container>
-      <Row>
-        <Col md={{ span: 3, offset: 3 }}>{"md={{span: 3, offset: 3}}"}</Col>
-      </Row>
-    </Container>
+   <div className = "d-flex flex-row justify-content-between">
+    <div className="intro_to_pg">Welcome to our page!</div>
+    <div className="image_of_taco">Add the Taco Here.</div>
+   </div>
   );
 }
 
-const exampleFunc = () => {
+/*const exampleFunc = () => {
   return (
     <div className="d-flex flex-row justify-content-between">
       <div className="display-1">Content 1</div>
       <div className="display-1">Content 2</div>
     </div>
   );
-};
+};*/
 
 export const HomePage = () => {
-  return <div>Home page works!</div>;
+  return blockofTitle();
+
 };
+
+
+// npm start
