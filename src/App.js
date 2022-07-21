6e25e269ea90a5bb626fd04bb341d0c359d12582
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-21 13:41:29 -0500

    Added margin to top of pages based off nav height

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import { Route, Routes } from "react-router-dom";
 import { NavProvider } from "./contexts/NavigationContext";
 import { Navbar } from "./components/Navbar/Navbar";
 import { HomePage } from "./pages/home";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const navRef = useRef(); // Ref to the container for the navbar
+  let navHeight = useRef(0); // Stores the height of the navbar
+
+  // Get the height of the navbar once it is rendered and store the new height
+  useEffect(() => {
+    const newNavHeight =
+      navRef.current?.children[0].getBoundingClientRect().height;
+    if (newNavHeight !== undefined) navHeight.current = newNavHeight;
+    console.log(newNavHeight);
+  }, [navRef]);
+
   return (
     <NavProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="home" element={<HomePage />} />
-      </Routes>
+      <div ref={navRef}>
+        <Navbar />
+      </div>
+      <div style={{ marginTop: `${navHeight.current}px` }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+        </Routes>
+      </div>
     </NavProvider>
   );
 }
