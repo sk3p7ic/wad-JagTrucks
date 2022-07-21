71e7a5828b8acc1dec39949a6977cdfd66eb433c
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-21 13:14:57 -0500

    Active page now underlined in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,10 @@
 import { Nav, Navbar as BSNavbar } from "react-bootstrap";
+import { useNavigation } from "../../contexts/NavigationContext";
 import "./Navbar.css";
 
 export const Navbar = () => {
+  const { currentPage, setCurrentPage } = useNavigation();
+
   return (
     <BSNavbar
       className="px-4 nav-black-text font-oswald d-flex flex-lg-row align-items-baseline"
@@ -14,9 +17,15 @@ export const Navbar = () => {
       <BSNavbar.Toggle aria-controls="main-nav" />
       <BSNavbar.Collapse id="main-nav">
         <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/schedule">Schedule</Nav.Link>
-          <Nav.Link href="/trucks">Trucks</Nav.Link>
+          <Nav.Link href="/" active={currentPage === ("home" || "/")}>
+            Home
+          </Nav.Link>
+          <Nav.Link href="/schedule" active={currentPage === "schedule"}>
+            Schedule
+          </Nav.Link>
+          <Nav.Link href="/trucks" active={currentPage === "trucks"}>
+            Trucks
+          </Nav.Link>
         </Nav>
         <hr className="d-lg-none" />
         <Nav>
