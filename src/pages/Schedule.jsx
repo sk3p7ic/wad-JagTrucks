72b6a872d865ab19113d149491f945c9af725782
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-26 09:27:35 -0500

    Started working on calendar display

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,14 +1,16 @@
 import { Container } from "react-bootstrap";
 import { ScheduleCalendarProvider } from "../contexts/ScheduleCalendarContext";
 import { ScheduleCalendarButtons } from "../components/Schedule/ScheduleCalendarButtons";
+import { ScheduleCalendar } from "../components/Schedule/ScheduleCalendar";
 
 export const SchedulePage = () => {
   return (
     <ScheduleCalendarProvider>
       <Container>
         <h1 className="display-1 font-oswald">What's Cookin'?</h1>
-        <div className="d-flex flex-column px-4">
+        <div className="d-flex flex-column gap-4 px-4">
           <ScheduleCalendarButtons />
+          <ScheduleCalendar />
         </div>
       </Container>
     </ScheduleCalendarProvider>
