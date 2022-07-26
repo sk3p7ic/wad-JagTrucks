dfa252ac602ae622195f34de8baa7adb62850760
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-26 12:10:37 -0500

    Added way to go back to main truck page

--- src/pages/trucks/TruckView.jsx
+++ src/pages/trucks/TruckView.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Button, Container } from "react-bootstrap";
+import { BsArrowLeftCircleFill } from "react-icons/bs";
+import { Link, useParams } from "react-router-dom";
 import { useNavigation } from "../../contexts/NavigationContext";
 
 /* TODO:
@@ -16,5 +18,12 @@ export const TruckViewPage = () => {
     return unsubscribe;
   }, [setCurrentPage]);
 
-  return <h1>Truck listing for {truckId}</h1>;
+  return (
+    <Container>
+      <Link to="/trucks">
+        <BsArrowLeftCircleFill size="48" />
+      </Link>
+      <h1>Truck listing for {truckId}</h1>
+    </Container>
+  );
 };
