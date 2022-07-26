290fa58a83086b68c015ede32e3e50d7e81cf0ac
Ryan Kasper <rhinodino5628@gmail.com>
2022-07-26 17:30:32 -0500

    Image viewable.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,13 @@
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
-import taco from '../../public/img/Tacos.png';
+//import taco from '../../public/img/Tacos.png';
 function blockofTitle() {
   return (
     <div className="flex-grow-1 d-flex flex-row justify-content-between">
       <div className="intro_to_pg">Welcome to our page!</div>
       {/*<div className="image_of_taco">Add the Taco Here.</div>*/}
-      <img src={taco} alt="image of tacos"/>
+      <img src="img/Tacos.png" alt="image of tacos"/>
     </div>
   );
 }
