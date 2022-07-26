dbe53a61dd62b90323f9e111b6e188cb6fbf593b
Ryan Kasper <rhinodino5628@gmail.com>
2022-07-26 12:17:30 -0500

    Tried adding the photo of the Tacos.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,13 @@
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
-
+import taco from '../public/img/taco.png';
 function blockofTitle() {
   return (
     <div className="flex-grow-1 d-flex flex-row justify-content-between">
       <div className="intro_to_pg">Welcome to our page!</div>
       <div className="image_of_taco">Add the Taco Here.</div>
+      <img src={taco} alt="image of tacos"/>
     </div>
   );
 }
