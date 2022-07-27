b62187196e6538eab04e965037f10ca98b4bd33a
Ryan Kasper <rhinodino5628@gmail.com>
2022-07-27 18:00:25 -0500

    Added Intro and buttons. Bug fix needed.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,22 @@
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import Button from 'react-bootstrap/Button';
 //import taco from '../../public/img/Tacos.png';
 function blockofTitle() {
   return (
     <div className="flex-grow-1 d-flex flex-row justify-content-between">
-      <div className="intro_to_pg">Welcome to our page!</div>
+      <div className="intro_to_pg" style={{padding:100}}>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h1>Dining on campus,<br></br>Made simple.</h1>
+        <h3>Explore the options available today.</h3>
+        <Button variant="success">What's Here Today </Button>
+        <Button variant="secondary">View All Trucks </Button>
+      </div>
       {/*<div className="image_of_taco">Add the Taco Here.</div>*/}
       <img src="img/Tacos.png" width={450} height={750}  alt="image of tacos"/>
     </div>
