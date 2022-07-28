839f28a877fb0588f788087ddecfe6d53a25399d
Ryan Kasper <rhinodino5628@gmail.com>
2022-07-28 16:25:25 -0500

    Fixed tacos.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,22 +5,19 @@ import Button from 'react-bootstrap/Button';
 //import taco from '../../public/img/Tacos.png';
 function blockofTitle() {
   return (
-    <div className="flex-grow-1 d-flex flex-row justify-content-between">
-      <div className="intro_to_pg" style={{padding:100}}>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+    <div className="flex-grow-1 d-flex flex-row justify-content-between align-items-center">
+      <div className="container-lg">
+        
         <h1 className="display-1 font-oswald">Dining on campus,<br></br>Made simple.</h1>
         <h5 className="display-5 font-vollkorn">Explore the options available today.</h5>
         <div>
           <Button variant="success" size="lg" href="/Schedule">What's Here Today </Button> {''}
-          <Button variant="outline-success" size="lg" href="/Trucks" >View All Trucks </Button>
+          <Button variant="outline-success" size="lg" href="/Trucks">View All Trucks </Button>
         </div>
 
       </div>
       {/*<div className="image_of_taco">Add the Taco Here.</div>*/}
-      <img src="img/Tacos.png" width={450} height={750}  alt="image of tacos"/>
+      <img src="img/Tacos.png" style={{height:"75vh"}} className="d-none d-lg-block" alt="image of tacos"/>
     </div>
   );
 }
