348354255f1de6593b16f8b98a2abc73718e70cd
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-28 22:58:05 -0500

    Added times to cards

--- src/components/Schedule/DayCards.jsx
+++ src/components/Schedule/DayCards.jsx
@@ -14,10 +14,31 @@ const DayCard = ({ scheduleItem, truckData }) => {
       : `/images/${truck.header_img_path}`;
   };
 
+  const getTruckTimes = () => {
+    const formatTimeString = (timeString) => {
+      let hours = parseInt(timeString.substring(0, 2));
+      let minutes = parseInt(timeString.substring(2));
+      const period = hours > 12 ? "pm" : "am";
+      hours = hours % 12;
+      hours = hours ? hours : 12; // Change hour 0 (false) to 12
+      minutes = minutes < 10 ? `0${minutes}` : minutes;
+      return `${hours}:${minutes} ${period}`;
+    };
+    const start = scheduleItem?.start_time | "";
+    const end = scheduleItem?.end_time | "";
+    if (start === "" || end === "") return <p></p>;
+    return (
+      <p>
+        {formatTimeString(start.toString())}-{formatTimeString(end.toString())}
+      </p>
+    );
+  };
+
   return (
     <Card>
       <Card.Img src={getTruckImageUrl(scheduleItem.truck_id)} />
       <Card.Title>{getTruckName(scheduleItem.truck_id)}</Card.Title>
+      <Card.Body>{getTruckTimes()}</Card.Body>
     </Card>
   );
 };
