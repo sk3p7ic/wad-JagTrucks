2df8fb9f1dc540750b1d964e17eed71cacc12102
Ryan Kasper <rhinodino5628@gmail.com>
2022-07-29 16:05:05 -0500

    Centering of sign in.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,27 +1,25 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Container, Button, Form} from 'react-bootstrap';
 
 export function FoodTruckLogin(){
     return(
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container className=" d-flex flex-column justify-content-centers align-items-center" >
+        <Form style= {{width:"fit-content"}}>  {/*className = "w-50vh"*/}
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+             <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
         </Form>
+    </Container>
     );
 }
