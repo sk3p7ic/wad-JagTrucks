963143cb2931e133dc69b6a178c6190b9a578249
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-28 22:36:06 -0500

    Added images to cards

--- src/components/Schedule/DayCards.jsx
+++ src/components/Schedule/DayCards.jsx
@@ -4,12 +4,19 @@ import { useNavigate } from "react-router-dom";
 const DayCard = ({ scheduleItem, truckData }) => {
   const getTruckName = (truckId) => {
     const truck = truckData.get(truckId);
-    console.log(truck);
     return truck === undefined || null ? <h2></h2> : <h2>{truck.name}</h2>;
+  };
+
+  const getTruckImageUrl = (truckId) => {
+    const truck = truckData.get(truckId);
+    return truck === undefined || null
+      ? ""
+      : `/images/${truck.header_img_path}`;
   };
 
   return (
     <Card>
+      <Card.Img src={getTruckImageUrl(scheduleItem.truck_id)} />
       <Card.Title>{getTruckName(scheduleItem.truck_id)}</Card.Title>
     </Card>
   );
