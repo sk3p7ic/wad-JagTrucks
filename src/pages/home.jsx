fed00387a7c4634ce204cff3cabc02727d500cb1
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-30 10:08:43 -0500

    Worked on styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,32 +1,37 @@
 import { Button } from "react-bootstrap";
+import "./Home.css";
 
 export const HomePage = () => {
   return (
     <div className="flex-grow-1 d-flex flex-row justify-content-between align-items-center">
       <div className="container-lg">
-        <h1 className="display-1 font-oswald">
-          Dining on campus,<br></br>Made simple.
-        </h1>
-        <h5 className="display-5 font-vollkorn">
-          Explore the options available today.
-        </h5>
-        <div>
-          <Button variant="success" size="lg" href="/Schedule">
-            What's Here Today{" "}
-          </Button>{" "}
-          {""}
-          <Button variant="outline-success" size="lg" href="/Trucks">
-            View All Trucks{" "}
-          </Button>
+        <div style={{ width: "fit-content" }}>
+          <h1 className="display-1 font-oswald">
+            Dining on campus,
+            <br />
+            Made simple.
+          </h1>
+          <h5 className="display-5 font-vollkorn">
+            Explore the options available today.
+          </h5>
+          <div className="gap-2 button-grid">
+            <Button variant="success" size="lg" href="/Schedule">
+              What's Here Today
+            </Button>
+            <Button variant="outline-success" size="lg" href="/Trucks">
+              View All Trucks
+            </Button>
+            <Button
+              variant="secondary"
+              size="lg"
+              href="/login"
+              className="button-grid-span-2"
+            >
+              Food Truck Driver Login/Create Sign-In
+            </Button>
+          </div>
         </div>
-        <br></br>
-        <Button variant="secondary" size="lg" href="/login">
-          {" "}
-          Food Truck Driver Login/Create Sign-In{" "}
-        </Button>{" "}
-        {""} {/*For a visible button.*/}
       </div>
-      {/*<div className="image_of_taco">Add the Taco Here.</div>*/}
       <div className="d-none d-lg-block position-relative">
         <img src="img/Tacos.png" style={{ height: "75vh" }} alt="Header" />
         <h1
@@ -35,9 +40,9 @@ export const HomePage = () => {
             position: "absolute",
             zIndex: -10,
             top: "50%",
-            left: "-100%",
+            left: "-125%",
             transform: "translateY(-50%)",
-            fontSize: "16rem",
+            fontSize: "14rem",
             color: "#fffbeb",
           }}
         >
