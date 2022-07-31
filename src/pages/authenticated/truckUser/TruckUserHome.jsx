12efd6fea2fa150b13bb01b676d8eafddf2d4f48
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-31 13:27:32 -0500

    Barebones for account homepage using acct status

--- src/pages/authenticated/truckUser/TruckUserHome.jsx
+++ src/pages/authenticated/truckUser/TruckUserHome.jsx
@@ -1,3 +1,47 @@
+import { useJagTrucksAuthentication } from "../../../contexts/AuthenticationContext";
+
+const AccountPendingPage = () => {
+  return (
+    <div className="flex-grow-1 d-flex flex-column gap-4 justify-content-center align-items-center text-center">
+      <h1 className="display-1 font-oswald">
+        Sorry, but your account is still marked as pending.
+      </h1>
+      <h1 className="font-vollkorn">
+        Be sure to check back later for updates!
+      </h1>
+    </div>
+  );
+};
+
+const AccountInvalidPage = () => {
+  return (
+    <div className="flex-grow-1 d-flex flex-column gap-4 justify-content-center align-items-center text-center">
+      <h1 className="display-1 font-oswald">
+        Sorry, but your account has been marked as{" "}
+        <span className="text-danger">INVALID</span>.
+      </h1>
+      <h1 className="font-vollkorn">
+        Please contact{" "}
+        <a href="mailto:truck.manager@jagtrucks.com" className="text-dark">
+          truck.manager@jagtrucks.com
+        </a>{" "}
+        if you believe this is an error.
+      </h1>
+    </div>
+  );
+};
+
+const AccountActivePage = () => {
+  return <div className="flex-grow-1">Active</div>;
+};
+
 export const TruckUserHome = () => {
-  return <div>TruckUserHome works!</div>;
+  const { jagTrucksAuth } = useJagTrucksAuthentication();
+  return (
+    <>
+      {jagTrucksAuth.accountStatus === "pending" && <AccountPendingPage />}
+      {jagTrucksAuth.accountStatus === "invalid" && <AccountInvalidPage />}
+      {jagTrucksAuth.accountStatus === "active" && <AccountActivePage />}
+    </>
+  );
 };
