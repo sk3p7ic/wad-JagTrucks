31d54f056860169ddadb2c42da742ade6b4875b3
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-31 10:12:38 -0500

    Added user menu link to go to profile

--- src/components/TruckUserPages/UserNavabarSection.jsx
+++ src/components/TruckUserPages/UserNavabarSection.jsx
@@ -6,6 +6,10 @@ export const UserNavbarSection = () => {
   const { jagTrucksAuth, logout } = useJagTrucksAuthentication();
   const routerNavigate = useNavigate();
 
+  const handleProfileClick = () => {
+    routerNavigate("/user/home");
+  };
+
   const handleLogout = () => {
     logout();
     routerNavigate("/login");
@@ -17,7 +21,14 @@ export const UserNavbarSection = () => {
         title={`${jagTrucksAuth.firstName} ${jagTrucksAuth.lastName}`}
         id="navProfileDropdown"
       >
-        <NavDropdown.Item eventKey="1.1">Profile</NavDropdown.Item>
+        <NavDropdown.Item
+          eventKey="1.1"
+          onClick={() => {
+            handleProfileClick();
+          }}
+        >
+          Profile
+        </NavDropdown.Item>
         <NavDropdown.Item eventKey="1.2">My Trucks</NavDropdown.Item>
         <NavDropdown.Item eventKey="1.3">Foo Bar</NavDropdown.Item>
         <NavDropdown.Divider />
