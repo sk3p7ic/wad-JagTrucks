e794ee6ee4b9b96926c588f0785143065155b94d
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-01 11:18:03 -0500

    Reformatted truck user dashboard

--- src/pages/authenticated/truckUser/TruckUserDashboard.jsx
+++ src/pages/authenticated/truckUser/TruckUserDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
-import { TruckDisplayCard } from "../../../components/Trucks/TruckDisplayCard";
+import { Accordion, Card, Container } from "react-bootstrap";
+import { TruckSocial } from "../../../components/SocialDisplay";
 import { useJagTrucksAuthentication } from "../../../contexts/AuthenticationContext";
 import { getTruckDataFromId } from "../../../util/db/trucks";
 
@@ -28,11 +28,38 @@ export const AccountActivePage = () => {
           Hello, {jagTrucksAuth.firstName}.
         </h1>
         <h1 className="font-oswald">Account Dashboard</h1>
-        <div>
-          <h2>Your Trucks</h2>
-          {ownedTrucks.map((truck) => (
-            <TruckDisplayCard truck={truck} key={truck._id} />
+        <div className="p-4 d-flex flex-column gap-2">
+          <h2 className="font-vollkorn">Your Trucks</h2>
+          {ownedTrucks.map((truck, index) => (
+            <Card className="p-2 font-nunito" key={index}>
+              <Card.Title className="font-oswald">{truck.name}</Card.Title>
+              <Card.Body className="d-flex flex-column flex-lg-row gap-4 pb-0">
+                <p className="d-flex flex-row align-items-center gap-2">
+                  <strong>Primary Food Type: </strong>
+                  {truck.primary_food_type}
+                </p>
+                <p className="d-flex flex-row align-items-center gap-2">
+                  <strong>Accepts Dining Dollars: </strong>
+                  {truck.accepts_dining_dollars ? "Yes" : "No"}
+                </p>
+                <p className="d-flex flex-row align-items-center gap-2">
+                  <strong>Socials: </strong>
+                  {Object.entries(truck.socials).map(([site, url], index) => (
+                    <TruckSocial siteName={site} url={url} key={index} />
+                  ))}
+                </p>
+              </Card.Body>
+              <Card.Link style={{ paddingLeft: "var(--bs-card-spacer-y)" }}>
+                Edit
+              </Card.Link>
+            </Card>
           ))}
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Add Another Truck</Accordion.Header>
+              <Accordion.Body>Foo bar.</Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </div>
       </Container>
     </div>
