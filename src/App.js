8f93e63ed6e343126889147e99d9b9ba331567b5
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-01 09:06:19 -0500

    Added cookie warning

--- src/App.js
+++ src/App.js
@@ -12,12 +12,42 @@ import { NewFoodTruck } from "./pages/newUser";
 import { JagTrucksAuthenticationProvider } from "./contexts/AuthenticationContext";
 import { RequireJagTrucksAuth } from "./pages/authenticated/RequireJagTrucksAuth";
 import { TruckUserHome } from "./pages/authenticated/truckUser/TruckUserHome";
+import { useEffect, useState } from "react";
+import { Alert } from "react-bootstrap";
 function App() {
+  const [showCookieWarning, setShowCookieWarning] = useState(false);
+
+  useEffect(() => {
+    const showCookieFromStorage = JSON.parse(
+      localStorage.getItem("doUseCookies")
+    );
+    if (!showCookieFromStorage) setShowCookieWarning(true);
+  }, []);
+
+  const onCookieWarningDismiss = () => {
+    localStorage.setItem("doUseCookies", JSON.stringify({ useCookies: true }));
+    setShowCookieWarning(false);
+  };
+
   return (
     <JagTrucksAuthenticationProvider>
       <NavProvider>
         <div className="vh-100 d-flex flex-column">
           <Navbar />
+          <Alert
+            variant="light"
+            className="position-fixed bottom-0 start-0 end-0 mx-auto"
+            style={{ width: "fit-content" }}
+            show={showCookieWarning}
+            onClose={() => onCookieWarningDismiss()}
+            dismissible
+          >
+            <Alert.Heading>This site uses cookies.</Alert.Heading>
+            <p>
+              This site uses cookies. By using this website you agree to use
+              cookies.
+            </p>
+          </Alert>
           <div className="flex-grow-1 overflow-auto d-flex flex-column">
             <Routes>
               <Route path="/" element={<HomePage />} />
